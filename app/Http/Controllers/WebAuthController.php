f6a01660400cbc28a020f92f25a678c4fdb62472
borjejanylle <152931520+borjejanylle@users.noreply.github.com>
2024-03-11 02:00:43 +0800

    Update WebAuthController.php

--- app/Http/Controllers/WebAuthController.php
+++ app/Http/Controllers/WebAuthController.php
@@ -43,27 +43,25 @@ class WebAuthController extends Controller
 
         return view('Auth.login');
     }
-    public function Authlogin(Request $request){
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required|min:5|max:20'
-        ]);
-    
-        $remember = !empty($request->remember);
-    
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password], $remember)) {
-            if(Auth::user()->user_type == 1){
-                return redirect('admin/dashboard');
-            }
-            elseif(Auth::user()->user_type == 3){
-                return redirect('student/dashboard');
-            }
+    public function Authlogin(Request $request)
+{
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required|min:5|max:20'
+    ]);
 
-        } 
-        else {
-            return back()->with('fail', 'Incorrect Email or Password');
+    $remember = !empty($request->remember);
+
+    if (Auth::attempt(['email' => $request->email, 'password' => $request->password], $remember)) {
+        if (Auth::user()->user_type == 1) {
+            return redirect('admin/dashboard');
+        } elseif (Auth::user()->user_type == 3) {
+            return redirect('student/dashboard');
         }
+    } else {
+        return redirect()->back()->with('fail', 'Incorrect Email or Password');
     }
+}
 
     public function registration(){
         return view("Auth.register");
